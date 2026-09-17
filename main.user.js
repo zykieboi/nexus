8053cc4d313ee251294c37bf08fb8f20861a64da
Zyk <basesukid@gmail.com>
2026-09-17 21:53:29 +0200

    Bump version to 1.0.2

--- main.user.js
+++ main.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Nexus - NX
 // @namespace    https://github.com/zykieboi/nexus
-// @version      1.0.1
+// @version      1.0.2
 // @icon         https://github.com/zykieboi/nexus/blob/main/img/icon.png?raw=true
 // @author       zykieboi
 // @description  Testing stuff :)
